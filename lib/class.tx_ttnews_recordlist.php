d423541a2256262dfabb007cbc20c098e15e950b
rupi <rupi@gmx.li>
2013-07-22 16:24:58 +0200

    [BUGFIX] re-added some require_once() lines, because of problems with TYPO3 6.0 and 6.1
    Change-Id: I178237bb72802ce15d13043617a6ccbba0d2792e
    Reviewed-on: https://review.typo3.org/22495
    Reviewed-by: Rupert Germann
    Tested-by: Rupert Germann

--- lib/class.tx_ttnews_recordlist.php
+++ lib/class.tx_ttnews_recordlist.php
@@ -60,6 +60,11 @@
  * (This index is automatically created/updated by the extension "extdeveval")
  *
  */
+require_once(PATH_t3lib.'class.t3lib_recordlist.php');
+require_once(PATH_typo3.'class.db_list.inc');
+
+require_once(t3lib_extMgm::extPath('cms').'layout/class.tx_cms_layout.php');
+
 
 	/**
 	 * [Describe function...]
